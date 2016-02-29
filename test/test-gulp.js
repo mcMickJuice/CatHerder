9bac771e32fea5c50300a7e539e993f7c8607bc9
mcMickJuice <mikejoyce19@gmail.com>
2016-02-29 17:04:05 -0600

    trying to run webpack + mocha in a gulp task. maybe this will work?

--- test/test-gulp.js
+++ test/test-gulp.js
@@ -5,43 +5,47 @@ var backendConfig = require('../webpack.backend.js');
 var rimraf = require('rimraf');
 var webpack = require('webpack');
 
-function backendTestBuild(testOutput, done) {
+function backendTestBuild(testConfig, done) {
     //change output to test file output
-    backendConfig.output = testOutput;
+    backendConfig = Object.assign({}, backendConfig, testConfig);
+    console.log('backendTestBuild', backendConfig)
 
-    webpack(backendConfig).run((err, stats => {
-        if(err) {
+    webpack(backendConfig).run((err, stats) => {
+        if (err) {
             console.log('error in backend test build!', err);
             process.exit();
         }
 
         done();
-    }));
+    });
 }
 
 function runTest(testFile, done) {
+    console.log('run test', testFile);
     var options = {
         files: [testFile]
     };
 
-    mocha(options, function() {
+    mocha(options, function () {
         done();
     })
 }
 
 function cleanTests(directoryPath) {
+    console.log('clean tests', directoryPath)
     rimraf(directoryPath);
 }
 
-gulp.task('test-backend', function() {
+gulp.task('test-backend', function () {
     var outputOptions = {
-        filename: 'backend-test.js'
+        entry: [path.resolve(__dirname, './backend-tests.index.js')],
+        output: {
+            filename: 'backend-test.js',
+            path: path.resolve(__dirname, 'testOutput')
+        }
     };
 
-    var testfileOutput = path.resolve(__dirname, 'testOutput');
-    var testFile =
-    backendTestBuild({
-        filename: 'backend-test.js',
-        path: testfileOutput
-    }, runTest(testFile, cleanTests(testfileOutput)));
+    backendTestBuild(outputOptions,
+        () => runTest(path.resolve(outputOptions.output.path, outputOptions.output.filename))
+    , () => cleanTests(outputOptions.output.path));
 });
