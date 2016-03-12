72b5d5bcfc3b80b627aa7257a6672bd05d4b78bd
Mike Joyce <mikejoyce19@gmail.com>
2016-03-11 20:39:15 -0600

    added linting to project

--- gulpfile-tests.js
+++ gulpfile-tests.js
@@ -7,66 +7,66 @@ var webpack = require('webpack');
 var Q = require('q');
 
 function backendTestBuild(testConfig) {
-    //change output to test file output
-    var deferred = Q.defer();
-    backendConfig = Object.assign({}, backendConfig, testConfig);
+  //change output to test file output
+  var deferred = Q.defer();
+  backendConfig = Object.assign({}, backendConfig, testConfig);
 
-    webpack(backendConfig).run((err, stats) => {
-        if (err) {
-            deferred.reject(err);
-            return;
-        }
+  webpack(backendConfig).run((err, stats) => {
+    if (err) {
+      deferred.reject(err);
+      return;
+    }
 
 
-        deferred.resolve();
-    });
+    deferred.resolve();
+  });
 
-    return deferred.promise;
+  return deferred.promise;
 }
 
 function runTest(testFile) {
-    var mochaRunner = new Mocha();
-    var deferred = Q.defer();
-    //mocha.addFile is an option too
-    mochaRunner.files = [testFile];
-    mochaRunner.run(() => deferred.resolve());
+  var mochaRunner = new Mocha();
+  var deferred = Q.defer();
+  //mocha.addFile is an option too
+  mochaRunner.files = [testFile];
+  mochaRunner.run(() => deferred.resolve());
 
-    return deferred.promise;
+  return deferred.promise;
 }
 
 function cleanTests(directoryPath) {
-    var deferred = Q.defer();
-    rimraf(directoryPath, {}, () => deferred.resolve());
+  var deferred = Q.defer();
+  rimraf(directoryPath, {}, () => deferred.resolve());
 
-    return deferred.promise;
+  return deferred.promise;
 }
 
 gulp.task('test-backend', function (done) {
-    var outputOptions = {
-        entry: path.resolve(__dirname,'./test/backend-web-app/backend-tests.index.js'),
-        output: {
-            filename: 'backend-test.js',
-            path: path.join(__dirname, './test/testOutput')
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.js$/,
-                    exclude: [
-                        /node_modules/
-                    ],
-                    loader: 'babel',
-                    query: {presets: ['es2015'], plugins: ['babel-plugin-rewire']}
-                }
-            ]
-        },
-    };
+  var outputOptions = {
+    entry: path.resolve(__dirname, './test/backend-web-app/backend-tests.index.js'),
+    output: {
+      filename: 'backend-test.js',
+      path: path.join(__dirname, './test/testOutput')
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: [
+            /node_modules/
+          ],
+          loader: 'babel',
+          query: {presets: ['es2015'], plugins: ['babel-plugin-rewire']}
+        }
+      ]
+    },
+  };
 
-    backendTestBuild(outputOptions)
-        .then(() => runTest(path.resolve(outputOptions.output.path, outputOptions.output.filename)))
-        .then(() => cleanTests(outputOptions.output.path))
-        .then(() => done())
-        .catch(err => {
-            console.log('Error in test pipeline', err, err.stack);
-        });
+  backendTestBuild(outputOptions)
+    .then(() => runTest(path.resolve(outputOptions.output.path, outputOptions.output.filename)))
+    .then(() => cleanTests(outputOptions.output.path))
+    .then(() => done())
+    .catch(err => {
+      console.log('Error in test pipeline', err, err.stack);
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var runSequence = require('run-sequence');
 var path = require('path');
+var eslint = require('gulp-eslint');
 var nodemon = require('nodemon');
 var rimraf = require('rimraf');
 var args = require('yargs').argv;
@@ -16,107 +17,119 @@ var backendConfig = require('./webpack.backend.js')(process.env.NODE_ENV);
 var frontendConfig = require('./webpack.frontend.js');
 
 gulp.task('clean', function (done) {
-    rimraf('./build', {}, () => done());
+  rimraf('./build', {}, () => done());
 });
 
 gulp.task('move-static', function () {
-    return gulp.src('./src/frontend-app/index.html')
-        .pipe(gulp.dest('./build/static'));
+  return gulp.src('./src/frontend-app/index.html')
+    .pipe(gulp.dest('./build/static'));
 });
 
 function onBuild(done) {
-    return function (err, stats) {
-        if (err) {
-            console.log('Error', err);
-        }
-        else {
-            console.log('Webpack Finished')
-        }
-
-        if (done) done();
+  return function (err, stats) {
+    if (err) {
+      console.log('Error', err);
     }
+    else {
+      console.log('Webpack Finished')
+    }
+
+    if (done) done();
+  }
 }
 
+var eslintGlob = args.glob || '*.js';
+gulp.task('lint', function() {
+
+  return gulp.src([eslintGlob, '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('lint-watch', ['lint'], function() {
+  gulp.watch(eslintGlob, ['lint']);
+});
+
 gulp.task('frontend-build', function (done) {
-    webpack(frontendConfig).run(onBuild(done))
+  webpack(frontendConfig).run(onBuild(done))
 });
 
 gulp.task('backend-build', function (done) {
-    webpack(backendConfig).run(onBuild(done))
+  webpack(backendConfig).run(onBuild(done))
 });
 
 gulp.task('build', ['frontend-build', 'backend-build'], function () {
-    console.log('Apps Built!')
+  console.log('Apps Built!')
 });
 
 gulp.task('frontend-watch', function () {
-    new WebpackDevServer(webpack(frontendConfig), {
-        publicPath: frontendConfig.output.publicPath,
-        stats: 'errors-only',
-    }).listen(3000, 'localhost', function (err, result) {
-        if (err) {
-            console.log('Error', err);
-        }
-        else {
-            console.log('webpack dev server listening at localhost:3000');
-        }
-    })
+  new WebpackDevServer(webpack(frontendConfig), {
+    publicPath: frontendConfig.output.publicPath,
+    stats: 'errors-only',
+  }).listen(3000, 'localhost', function (err, result) {
+    if (err) {
+      console.log('Error', err);
+    }
+    else {
+      console.log('webpack dev server listening at localhost:3000');
+    }
+  })
 });
 
 gulp.task('backend-watch', function (done) {
-    console.log(process.env.NODE_ENV)
-    //add hotplugin
-    var hotPlugin = new webpack.HotModuleReplacementPlugin({quiet: true});
-    backendConfig.plugins.push(hotPlugin);
-    //add signal.js to entry
-    var signalJsEntry = 'webpack/hot/signal.js';
-    if(backendConfig.entry instanceof Array && backendConfig.entry.indexOf(signalJsEntry) < 0) {
-        backendConfig.entry.unshift(signalJsEntry);
-    } else {
-        backendConfig.entry = [signalJsEntry, backendConfig.entry];
+  console.log(process.env.NODE_ENV)
+  //add hotplugin
+  var hotPlugin = new webpack.HotModuleReplacementPlugin({quiet: true});
+  backendConfig.plugins.push(hotPlugin);
+  //add signal.js to entry
+  var signalJsEntry = 'webpack/hot/signal.js';
+  if (backendConfig.entry instanceof Array && backendConfig.entry.indexOf(signalJsEntry) < 0) {
+    backendConfig.entry.unshift(signalJsEntry);
+  } else {
+    backendConfig.entry = [signalJsEntry, backendConfig.entry];
+  }
+
+  var firedDone = false;
+
+  var compiler = webpack(backendConfig);
+  //run initial compile
+  compiler.run((err, stats) => {
+    if (err) {
+      console.log('error on initial compile');
     }
 
-    var firedDone = false;
+    startServer();
+  });
 
-    var compiler = webpack(backendConfig);
-    //run initial compile
-    compiler.run((err, stats) => {
-        if(err) {
-            console.log('error on initial compile');
-        }
+  //watch files to reload
+  compiler.watch(100, function (err, stats) {
+    if (!firedDone) {
+      firedDone = true;
+      done();
+    }
 
-        startServer();
-    });
-
-    //watch files to reload
-    compiler.watch(100, function (err, stats) {
-        if (!firedDone) {
-            firedDone = true;
-            done();
-        }
-
-        nodemon.restart();
-    })
+    nodemon.restart();
+  })
 });
 
 function startServer() {
-    nodemon({
-        execMap: {
-            js: 'node'
-        },
-        script: path.resolve(__dirname, 'build/backend-web'),
-        //ignore everything, watch nothing. webpack watch will handle restarting
-        ignore: ['*'],
-        watch: ['foo/'],
-        ext: 'noop'
-    }).on('restart', function () {
-        console.log('Patched!!!')
-    })
+  nodemon({
+    execMap: {
+      js: 'node'
+    },
+    script: path.resolve(__dirname, 'build/backend-web'),
+    //ignore everything, watch nothing. webpack watch will handle restarting
+    ignore: ['*'],
+    watch: ['foo/'],
+    ext: 'noop'
+  }).on('restart', function () {
+    console.log('Patched!!!')
+  })
 }
 
 gulp.task('watch', function (done) {
-    runSequence('clean', 'move-static'
-        , ['backend-watch', 'frontend-watch']
-        , done);
+  runSequence('clean', 'move-static'
+    , ['backend-watch', 'frontend-watch']
+    , done);
 });
 
